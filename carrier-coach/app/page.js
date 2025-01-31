6380f50f768fe6e36a8e21b846d559822b22177c
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-01-31 14:46:46 +0530

    Created structure.

---
+++ carrier-coach/app/page.js
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+     <h1>Carrier Coach</h1>
+     <br/>
+     <Button>Click me</Button>
+   </div>
+  );
+
+}
