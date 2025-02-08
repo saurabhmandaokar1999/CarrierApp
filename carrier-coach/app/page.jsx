8c4c1f10e5ddd1789e4291648d6a1e9eead328ed
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-02-08 09:59:34 +0530

    Adding files.

--- carrier-coach/app/page.jsx
+++ carrier-coach/app/page.jsx
@@ -1,10 +1,39 @@
 import HeroSection from "@/components/hero";
+import { features } from "@/data/features";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
 export default function Home() {
   return (
     <div>
    <div className="grid-background"> 
    </div>
    <HeroSection />
+   <section className="w-full py-12 md:py-24 lg:py-32 bg-background">
+    <div className="container mx-auto px-4 md:px-6">
+      <h2 className="text-3xl font-bold tracking-tighter text-center mb-12">
+        Powerful features for your career growth.
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 max-w-6xl mx-auto">
+        {features.map((feature, index) => {
+        return (
+          <Card key={index}>
+            <CardContent>
+              <div>{feature.icon}</div>
+              <h3>{feature.title}</h3>
+              <p>{feature.description}</p>
+            </CardContent>
+          </Card>
+        )
+      })}</div>
+    </div>
+   </section>
    </div>
   );
 }
