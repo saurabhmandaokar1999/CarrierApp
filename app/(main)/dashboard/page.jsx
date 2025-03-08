2e4bf2cb09dc02a9a910dca8826922eb2e2c8e37
saurabh mandaokar <saurabh.s.mandaokar@gmail.com>
2025-03-08 10:52:38 +0530

    Making dashboard page

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,15 +1,22 @@
+import { getIndustryInsights } from "@/actions/dashboard";
+import DashboardView from "./_components/dashboard-view";
 import { getUserOnboardingStatus } from "@/actions/user";
 import { redirect } from "next/navigation";
 
-const IndustryInsightpage = async () => {
-    const {isOnboarded}=await getUserOnboardingStatus();
-    if(!isOnboarded){ 
-       redirect("/onboarding");
-    }
-    return (
-        <main>
-            <h1>Industry Insight</h1>
-        </main>
-    );
-};
-export default IndustryInsightpage;
+export default async function DashboardPage() {
+  const { isOnboarded } = await getUserOnboardingStatus();
+
+  // If not onboarded, redirect to onboarding page
+  // Skip this check if already on the onboarding page
+  if (!isOnboarded) {
+    redirect("/onboarding");
+  }
+
+  const insights = await getIndustryInsights();
+
+  return (
+    <div className="container mx-auto">
+      <DashboardView insights={insights} />
+    </div>
+  );
+}
